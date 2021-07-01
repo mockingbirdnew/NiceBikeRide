ac45206ba0ad7a8040d3af167e39a4bed464ef10
Ivan Kozhemyako <rgd_57@mail.ru>
2021-07-02 03:48:59 +0500

    Сделал верстку 3 экрана. Исправил синтаксические ошибки

---
+++ app/src/main/java/com/example/nicebikeride/UserChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.nicebikeride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_choice);
+    }
+}
