a01f20cce6b7ca0da2a59e339adc5cc8b01373f9
Ivan Kozhemyako <rgd_57@mail.ru>
2021-07-07 02:29:49 +0500

    начал добавлять ретрофит

--- app/src/main/java/com/example/nicebikeride/MainActivity.java
+++ app/src/main/java/com/example/nicebikeride/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit.Builder =(new Retrofit.Builder())
     }
 }
---
+++ app/src/main/java/com/example/nicebikeride/RetrofitService.java
@@ -0,0 +1,4 @@
+package com.example.nicebikeride;
+
+public interface RetrofitService {
+}
