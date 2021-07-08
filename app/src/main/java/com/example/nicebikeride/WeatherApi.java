5d47f21d04e317a3e8ef18a184214a72fcaad9cc
Ivan Kozhemyako <rgd_57@mail.ru>
2021-07-09 02:40:33 +0500

    09.07.2021 02.40

--- app/src/main/java/com/example/nicebikeride/WeatherApi.java
+++ app/src/main/java/com/example/nicebikeride/WeatherApi.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class WeatherApi extends AppCompatActivity {
 
@@ -13,13 +17,20 @@ public class WeatherApi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_api);
-
+        initRetrofit();
     }
-    protected void initRetrofit(){
+    protected void initRetrofit() {
         OkHttpClient client = new OkHttpClient.Builder()
-                .build()
-        Retrofit retrofit= (new Retrofit.Builder())
+                .build();
+
+        Retrofit retrofit = (new Retrofit.Builder())
+                .baseUrl("https://weatherapi.com/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .client(client)
+                .build();
+
+        RetrofitService service = retrofit.create(RetrofitService.class);
+        }
 
     }
 
-}
